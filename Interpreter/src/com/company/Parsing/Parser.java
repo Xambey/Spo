6d1a83ee269b195da7a76df9a8481b17f0b5e6a2
Xambey <Xambey@yandex.ru>
2017-06-16 16:34:49 +0300

    saved

---
+++ Interpreter/src/com/company/Parsing/Parser.java
@@ -0,0 +1,23 @@
+package com.company.Parsing;
+
+import com.company.Tokens.Token;
+
+import java.util.List;
+
+public class Parser {
+    /*
+    *   Expr ->
+    */
+
+
+    private List<Token> tokens;
+    public Parser(List<Token> tokens)
+    {
+        this.tokens = tokens;
+    }
+    public Parser()
+    {
+
+    }
+
+}
